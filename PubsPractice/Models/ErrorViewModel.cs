758fb8aa08adcd30e6b2737c96241987cfad484d
PinHow <plowrain1328@gmail.com>
2025-08-07 11:00:16 +0800

    加入專案檔案。

---
+++ PubsPractice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PubsPractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
